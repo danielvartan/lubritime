6ea393d9921807818d449a1d20e52ec67d4149f8
Daniel Vartanian <danvartan@gmail.com>
2023-02-19 05:11:32 -0300

    Fix 'round_time()' documentation

--- R/round_time.R
+++ R/round_time.R
@@ -3,7 +3,7 @@
 #' @description
 #'
 #' `r lifecycle::badge("maturing")`
-#' 
+#'
 #' `round_time()` takes a [`Duration`][lubridate::duration()],
 #' [`difftime`][base::as.difftime()], [`hms`][hms::hms()],
 #' [`POSIXct`][base::as.POSIXct()], or [`POSIXlt`][base::as.POSIXlt()] object
@@ -86,10 +86,11 @@
 round_time <- function(x) {
     classes <- c("Duration", "difftime", "hms", "POSIXct", "POSIXlt")
     checkmate::assert_multi_class(x, classes)
-    
+
     UseMethod("round_time")
 }
 
+#' @rdname round_time
 #' @export
 round_time.Duration <- function(x) {
     x %>% as.numeric() %>%
@@ -97,20 +98,22 @@ round_time.Duration <- function(x) {
         lubridate::dseconds()
 }
 
+#' @rdname round_time
 #' @export
 round_time.difftime <- function(x) {
     out <- x
     units(out) <- "secs"
-    
+
     out <- out %>% as.numeric() %>%
         round() %>%
         as.difftime(units = "secs")
-    
+
     units(out) <- units(x)
-    
+
     out
 }
 
+#' @rdname round_time
 #' @export
 round_time.hms <- function(x) {
     x %>% as.numeric() %>%
@@ -118,28 +121,30 @@ round_time.hms <- function(x) {
         hms::as_hms()
 }
 
+#' @rdname round_time
 #' @export
 round_time.POSIXct <- function(x) {
     out <- x %>% as.numeric() %>%
         round()
-    
+
     attributes(out) <- attributes(x)
-    
+
     out
 }
 
+#' @rdname round_time
 #' @export
 round_time.POSIXlt <- function(x) {
     out <- unclass(x)
-    
+
     if (round(out$sec) >= 60) {
         out$sec <- round(out$sec) - 60
         out$min <- out$min + 1
     } else {
         out$sec <- round(out$sec)
     }
-    
+
     class(out) <- class(x)
-    
+
     out
 }
